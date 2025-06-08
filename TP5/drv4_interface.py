519d8016759fb7d79fabe38b6b1c733782e395fd
Ruben-Zuniga <ruben.zuniga@mi.unc.edu.ar>
2025-06-08 13:36:17 -0300

    add drv4 interface

---
+++ TP5/drv4_interface.py
@@ -0,0 +1,41 @@
+import time
+
+DEVICE = "/dev/SdeC_drv4"
+
+# Seleccionar puerto a leer
+def set_signal(idx):
+    with open(DEVICE, 'wb') as f:
+        # Escritura en el CDD
+        f.write(bytes(str(idx), 'ascii'))
+
+# Leer puerto seleccionado
+def read_sample():
+    with open(DEVICE, 'rb') as f:
+        # Lectura y decodificacion en el CDD
+        val = f.read(32).decode()
+    return int(val)
+
+# Plot
+def plot_loop():
+    # El usuario decide aquí cuál señal leer (0 o 1):
+    sel = int(input("¿Leer señal 0 o 1? Presione Ctrl+C para salir.\n"))
+    set_signal(sel)
+    ts = 0
+    print(f"Señal a leer: {sel}. Presione Ctrl+C para leer otra señal.")
+
+    while True:
+        try:
+            val = read_sample()
+            print(f"Tiepo: {ts}, Valor: {val}")
+            ts += 1
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sel = int(input("¿Leer señal 0 o 1? Presione Ctrl+C para salir.\n"))
+            set_signal(sel)
+            ts = 0
+
+def main():
+    plot_loop()
+
+if __name__ == "__main__":
+    main()
